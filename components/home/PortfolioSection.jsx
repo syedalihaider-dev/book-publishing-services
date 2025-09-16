285331e82888f588e25d08468b91f88c8040a385
alihaider-design <ali.haider@canvasdigital.org>
2025-09-16 23:53:44 +0500

    Add New Tabs in Portfolio Section

--- components/home/PortfolioSection.jsx
+++ components/home/PortfolioSection.jsx
@@ -1,4 +1,6 @@
 "use client";
+
+import { useState } from "react";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Navigation, Autoplay } from "swiper/modules";
@@ -7,58 +9,157 @@ import "swiper/css/navigation";
 import styles from "./PortfolioSection.page.module.css";
 
 export default function PortfolioSection() {
-  return (
-    <section className={`${styles.portfolio_section} sec_padding`}>
-      <div className="container">
-        <div className={`${styles.sec_top} text-center`}>
-          <div className="row">
-            <div className="col-sm-12 col-md-12">
-              <h2>
-                We Publish these
-                <span className="break_line"></span> 
-                <span className="primarytxt">Book Categories</span>
-              </h2>
-              <p>
-                At Book Publishing, we believe every story has the power to change 
-                <span className="break_line"></span> the world. We publish books that inspire, entertain.
-              </p>
-            </div>
-          </div>
-        </div>
-      </div>
-      
-      <Swiper
-        modules={[Navigation, Autoplay]}
-        loop={true}
-        slidesPerView={5}
-        spaceBetween={20}
-        centeredSlides={false}
-        autoplay={{
+const [activeGenre, setActiveGenre] = useState(0);
+
+const genres = [
+{
+name: "Fiction",
+books: [
+"/port-book-01.png",
+"/port-book-02.png",
+"/port-book-03.png",
+"/port-book-04.png",
+"/port-book-05.png",
+"/port-book-06.png",
+],
+},
+{
+name: "Non-Fiction",
+books: [
+"/port-book-02.png",
+"/port-book-03.png",
+"/port-book-04.png",
+"/port-book-05.png",
+"/port-book-06.png",
+"/port-book-01.png",
+],
+},
+{
+name: "Romance",
+books: [
+"/port-book-03.png",
+"/port-book-04.png",
+"/port-book-05.png",
+"/port-book-06.png",
+"/port-book-01.png",
+"/port-book-02.png",
+],
+},
+{
+name: "Science",
+books: [
+"/port-book-04.png",
+"/port-book-05.png",
+"/port-book-06.png",
+"/port-book-01.png",
+"/port-book-02.png",
+"/port-book-03.png",
+],
+},
+{
+name: "History",
+books: [
+"/port-book-05.png",
+"/port-book-06.png",
+"/port-book-01.png",
+"/port-book-02.png",
+"/port-book-03.png",
+"/port-book-04.png",
+],
+},
+{
+name: "Fantasy",
+books: [
+"/port-book-06.png",
+"/port-book-01.png",
+"/port-book-02.png",
+"/port-book-03.png",
+"/port-book-04.png",
+"/port-book-05.png",
+],
+},
+{
+name: "Biography",
+books: [
+"/port-book-01.png",
+"/port-book-03.png",
+"/port-book-05.png",
+"/port-book-02.png",
+"/port-book-04.png",
+"/port-book-06.png",
+],
+},
+{
+name: "Children",
+books: [
+"/port-book-02.png",
+"/port-book-04.png",
+"/port-book-06.png",
+"/port-book-01.png",
+"/port-book-03.png",
+"/port-book-05.png",
+],
+},
+{
+name: "Mystery",
+books: [
+"/port-book-03.png",
+"/port-book-05.png",
+"/port-book-01.png",
+"/port-book-06.png",
+"/port-book-02.png",
+"/port-book-04.png",
+],
+},
+];
+
+return (
+<section className={`${styles.portfolio_section} sec_padding`}>
+  <div className={`${styles.book} ${styles.book1}`}>
+    <Image src="/book-icon-01.png" width={147} height={121} alt="Book Images" className="myImages" />
+  </div>
+  <div className={`${styles.book} ${styles.book2}`}>
+    <Image src="/book-icon-02.png" width={141} height={123} alt="Book Images" className="myImages" />
+  </div>
+  <div className={`${styles.book} ${styles.book3}`}>
+    <Image src="/book-icon-03.png" width={161} height={124} alt="Book Images" className="myImages" />
+  </div>
+  <div className={`${styles.book} ${styles.book4}`}>
+    <Image src="/book-icon-04.png" width={123} height={114} alt="Book Images" className="myImages" />
+  </div>
+  <div className="container">
+    <div className={`${styles.sec_top} text-center`}>
+      <h2>
+        We Publish these
+        <span className="break_line"></span>
+        <span className="primarytxt">Book Categories</span>
+      </h2>
+      <p>
+        At Book Publishing, we believe every story has the power to change
+        <span className="break_line"></span> the world. We publish books that inspire, entertain.
+      </p>
+      <ul className={styles.tabs}>
+        {genres.map((genre, index) => (
+        <li key={index} className={index===activeGenre ? styles.activeTab : "" } onClick={()=> setActiveGenre(index)}
+          >
+          {genre.name}
+        </li>
+        ))}
+      </ul>
+    </div>
+  </div>
+
+  {/* Swiper for active genre */}
+  <Swiper modules={[Navigation, Autoplay]} loop={true} slidesPerView={5} spaceBetween={20} autoplay={{
           delay: 2500,
           disableOnInteraction: false,
-        }}
-        navigation={false}
-        className={styles.swiper}
-        >
-        <SwiperSlide>
-          <Image src="/port-book-01.png" alt="Portfolio Book" width={320} height={480} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src="/port-book-02.png" alt="Portfolio Book" width={320} height={480} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src="/port-book-03.png" alt="Portfolio Book" width={320} height={480} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src="/port-book-04.png" alt="Portfolio Book" width={320} height={480} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src="/port-book-05.png" alt="Portfolio Book" width={320} height={480} />
-        </SwiperSlide>
-        <SwiperSlide>
-          <Image src="/port-book-06.png" alt="Portfolio Book" width={320} height={480} />
-        </SwiperSlide>
-      </Swiper>
-    </section>
-  );
+        }} className={styles.swiper}>
+    {genres[activeGenre].books.map((book, i) => (
+    <SwiperSlide key={i}>
+      <Image src={book} alt="Portfolio Book" width={320} height={480} />
+    </SwiperSlide>
+    ))}
+  </Swiper>
+</section>
+);
 }
