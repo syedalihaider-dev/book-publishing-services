93cd9bc3fb7bdabf4de47ec281020e26c450855c
syedalihaider-dev <aleehaiderbalti@gmail.com>
2025-10-29 23:38:16 +0500

    Add Google Tag Manager Script

--- app/layout.js
+++ app/layout.js
@@ -1,7 +1,8 @@
 import { Lexend } from "next/font/google";
+import Script from "next/script";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./globals.css";
-import {Header, Footer, MarqueeSlider, Popup} from "@/components/layout";
+import { Header, Footer, MarqueeSlider, Popup } from "@/components/layout";
 import ChatWidget from "@/components/ChatWidget";
 // import AOSInitializer from "@/components/AOS/AOSInitializer";
 
@@ -39,7 +40,30 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        {/* ✅ Google Tag Manager Script */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+          new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+          j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+          'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+          })(window,document,'script','dataLayer','GTM-5WPMZ7WC');`}
+        </Script>
+        {/* ✅ End Google Tag Manager */}
+      </head>
+
       <body className={lexend.className}>
+        {/* ✅ Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5WPMZ7WC"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+        {/* ✅ End Google Tag Manager (noscript) */}
+
         <Header />
         {children}
         <Footer />
