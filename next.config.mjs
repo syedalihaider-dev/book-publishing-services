9b81faae414031bcefc8a3b8505566a8a42183f0
syedalihaider-dev <aleehaiderbalti@gmail.com>
2025-11-03 22:38:14 +0500

    Edit next.config.mjs File

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,19 @@
+// /** @type {import('next').NextConfig} */ 
+// const nextConfig = {
+
+// }; 
+// export default nextConfig;
+
 /** @type {import('next').NextConfig} */ 
 const nextConfig = {
+  async rewrites() {
+    return [
+      {
+        source: '/blog/:path*',
+        destination: 'https://bookpublishingservices.io.cdn.hstgr.net/blog/:path*',
+      },
+    ];
+  },
+};
 
-}; 
 export default nextConfig;
