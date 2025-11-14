d7a698a74c48f93a5c484d04b353bc26f657f14d
AbdullahSaeed <AbdullahSaeed@int.sybrid.com>
2025-11-14 23:35:23 +0500

    changes in next

--- next.config.mjs
+++ next.config.mjs
@@ -1,19 +1,19 @@
-// /** @type {import('next').NextConfig} */ 
-// const nextConfig = {
-
-// }; 
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */ 
 const nextConfig = {
-  async rewrites() {
-    return [
-      {
-        source: '/blog/:path*',
-        destination: 'https://darkviolet-jellyfish-929687.hostingersite.com/blog/:path*',
-      },
-    ];
-  },
-};
 
+}; 
 export default nextConfig;
+
+// /** @type {import('next').NextConfig} */ 
+// const nextConfig = {
+//   async rewrites() {
+//     return [
+//       {
+//         source: '/blog/:path*',
+//         destination: 'https://darkviolet-jellyfish-929687.hostingersite.com/blog/:path*',
+//       },
+//     ];
+//   },
+// };
+
+// export default nextConfig;
