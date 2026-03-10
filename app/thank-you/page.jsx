cac3efb8ad2eaddc253c8f1a650623aa2fdbeebf
syedalihaider-dev <aleehaiderbalti@gmail.com>
2026-03-11 00:42:43 +0500

    New Update

--- app/thank-you/page.jsx
+++ app/thank-you/page.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 export const metadata = {
-  title: "Thank You || Book Publishing Services",
+  title: "Thank You | Book Publishing Services",
   description:
     "Thank you for contacting Book Publishing Services. Our team will reach out shortly to discuss your publishing goals and guide you through the next steps.",
   openGraph: {
@@ -19,29 +19,28 @@ export const metadata = {
   },
 };
 
-
 export default function ThankYou() {
-return (
-<div className={styles.thank_you}>
-    <Image
+  return (
+    <div className={styles.thank_you}>
+      <Image
         src="/banner-bg.png"
         alt="Banner Bg Image"
         fill
         style={{ objectFit: "cover" }}
         className={styles.bannerBg}
-    />
-    <div className="container">
+      />
+      <div className="container">
         <div className="row">
-            <div className="col-sm-12 col-md-12">
-                <h1>Thank You</h1>
-                <p>
-                    Your form has been submitted successfully. Our team will get back to you shortly.
-                </p>
-                <hr />
-                <Link href="/" className={styles.btn}>Go Back to Home</Link>
-            </div>
+          <div className="col-sm-12 col-md-12">
+            <h1>Thank You</h1>
+            <p>
+              Your form has been submitted successfully. Our team will get back to you shortly.
+            </p>
+            <hr />
+            <Link href="/" className={styles.btn}>Go Back to Home</Link>
+          </div>
         </div>
+      </div>
     </div>
-</div>
-);
+  );
 }
